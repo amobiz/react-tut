42aa7ab17764bbf4a2e7693dd570c74b11b1045c
amobiz <amobiz.tw+github@gmail.com>
2016-05-06 21:37:12 +0800

    step 3: render component with given state

--- app/helloworld.js
+++ app/helloworld.js
@@ -1,10 +1,17 @@
 import React, {Component} from 'react';
 
 export default class HelloWorld extends Component {
+    constructor() {
+        super();
+        this.state = {
+            name: 'guest'
+        }
+    }
+
     render () {
         return (
             <p>
-                Hello, <input type="text" placeholder="Your name here" value="guest" />!
+                Hello, <input type="text" placeholder="Your name here" value={this.state.name} />!
                 It is {this.props.date.toTimeString()}
             </p>
         );
