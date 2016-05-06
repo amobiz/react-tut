d4e5883eb655e25a34ae82199ea7ee8f85462c76
amobiz <amobiz.tw+github@gmail.com>
2016-05-06 21:37:42 +0800

    step 4: add onChange handler to component

--- app/helloworld.js
+++ app/helloworld.js
@@ -8,10 +8,14 @@ export default class HelloWorld extends Component {
         }
     }
 
+    handleChange(event) {
+        this.setState({ [event.target.name]: event.target.value });
+    }
+
     render () {
         return (
             <p>
-                Hello, <input type="text" placeholder="Your name here" value={this.state.name} />!
+                Hello, <input type='text' placeholder='Your name here' name='name' value={this.state.name} onChange={this.handleChange.bind(this)} />!
                 It is {this.props.date.toTimeString()}
             </p>
         );
